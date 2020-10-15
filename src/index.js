b5957b1e67af9ae9ff914ecd184330c12db7aff6
Uladzislau Lisetski <lisetskiywlad@gmail.com>
2020-10-15 18:32:59 +0300

    init: start and completed task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,44 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+  	let ten = ''
+    let result_temp = ''
+    let result = ''
+    let symbols = ''
+
+
+    for(let i = 0; i < expr.length; i++)
+    {
+    	ten += expr[i]
+        if((i + 1) % 10 === 0)
+        {
+        	if(ten === '**********') result += ' '
+            else 
+            {            
+                for(let j = 0; j < ten.length; j++)
+                {            
+            	    result_temp += ten[j]
+                    if((j + 1) % 2 === 0)
+                    {
+                
+                        if(result_temp === '10') symbols += '.'
+                        if(result_temp === '11') symbols += '-'                   
+                    
+                        result_temp = ''
+                    }               
+                
+                }            
+                for(var index in MORSE_TABLE)
+    		    {
+        		    if(symbols === index) result += MORSE_TABLE[index]
+    		    }
+            }
+            ten = ''
+            symbols = ''
+        }
+    }
+
+    return result;
 }
 
 module.exports = {
